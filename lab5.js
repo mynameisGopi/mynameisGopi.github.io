77f07e1fd5018b7298dd56246c8894414a93522d
mynameisGopi <gthillairasa@torontomu.ca>
2024-02-26 00:36:48 -0500

    Lab 5 Submission
    Adding JS file and jpg as part of Lab 5 requirements.

---
+++ lab5.js
@@ -0,0 +1,35 @@
+/*Lab 5 Part A*/
+function myName(){
+    var name = prompt("Enter user name.")
+}
+/*Lab 5 Part B*/
+function myImg(){
+document.getElementById("Image").onclick = function() {
+};
+}
+/*Lab 5 Part C Menu*/
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+//Close the Dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i<dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+/*Lab 5 Part D*/
+function mOver(some_tag) {
+    some_tag.innerHTML = "Thank You";
+    some_tag.style.color = "blue";
+}
+function mOut(element) {
+    some_tag.innerHTML = "Mouse Over Me";
+    some_tag.style.color = "orange";
+}
